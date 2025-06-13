8ec61add445ea43d315cd7edafad88f3f996dbb6
ubing <ubing0101@kakao.com>
2025-06-13 12:26:49 +0900

    User.java - setter 추가

--- src/main/java/com/playdata/batchpractice/entity/User.java
+++ src/main/java/com/playdata/batchpractice/entity/User.java
@@ -9,9 +9,11 @@ import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.ToString;
 
-@Getter @ToString
+@Getter @Setter
+@ToString
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
